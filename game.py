6934b37fee1ba06d7a974020cb028144e7673e46
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-05-21 18:49:40 +0300

    version from gpt

--- game.py
+++ game.py
@@ -14,3 +14,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=443, ssl_context=(ssl_cert_path, ssl_key_path))
+    # app.run()
--- static/script.js
+++ static/script.js
@@ -1,12 +1,10 @@
-function startGame() {
-    // Выполняем AJAX-запрос к серверу Python
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/start_game", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            document.getElementById("gameResult").innerHTML = response.message;
-        }
-    };
-    xhr.send();
-}
+function changeColor() {
+    var btn = document.querySelector('.color-change-btn');
+    var currentColor = btn.style.backgroundColor;
+
+    if (currentColor === 'red') {
+      btn.style.backgroundColor = 'blue';
+    } else {
+      btn.style.backgroundColor = 'red';
+    }
+  }
