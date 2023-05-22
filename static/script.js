471258190079df1cf67e2e87969221ec6d8ea7c2
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-05-23 00:08:25 +0300

    some changes in html game

--- static/script.js
+++ static/script.js
@@ -1,10 +1,69 @@
-function changeColor() {
-    var btn = document.querySelector('.color-change-btn');
-    var currentColor = btn.style.backgroundColor;
+document.addEventListener("DOMContentLoaded", () => {
+  const player = document.getElementById("player");
+  const gameContainer = document.getElementById("game-container");
 
-    if (currentColor === 'red') {
-      btn.style.backgroundColor = 'blue';
-    } else {
-      btn.style.backgroundColor = 'red';
-    }
+  let playerX = gameContainer.offsetWidth / 2 - player.offsetWidth / 2;
+
+  document.addEventListener("keydown", (event) => {
+      if (event.code === "ArrowLeft") {
+          playerX -= 25;
+      } else if (event.code === "ArrowRight") {
+          playerX += 25;
+      }
+
+      // Ограничение движения игрока в пределах контейнера
+      if (playerX < 0) {
+          playerX = 0;
+      } else if (playerX > gameContainer.offsetWidth - player.offsetWidth) {
+          playerX = gameContainer.offsetWidth - player.offsetWidth;
+      }
+
+      player.style.left = playerX + "px";
+  });
+
+  function createFallingObject() {
+      const object = document.createElement("div");
+      object.className = "falling-object";
+
+      const randomX = Math.floor(Math.random() * (gameContainer.offsetWidth - 20));
+      object.style.left = randomX + "px";
+      object.style.top = "0px";
+
+      gameContainer.appendChild(object);
+
+      const fallingObjectInterval = setInterval(() => {
+          const objectTop = object.offsetTop;
+          const objectBottom = object.offsetTop + object.offsetHeight;
+          const objectLeft = object.offsetLeft;
+          const objectRight = object.offsetLeft + object.offsetWidth;
+
+          const playerTop = player.offsetTop;
+          const playerBottom = player.offsetTop + player.offsetHeight;
+          const playerLeft = player.offsetLeft;
+          const playerRight = player.offsetLeft + player.offsetWidth;
+
+          if (
+              ((playerTop <= objectTop && objectTop <= playerBottom) ||
+              (playerTop <= objectBottom && objectBottom <= playerBottom)) &&
+              (playerLeft <= objectRight && objectRight <= playerRight ||
+              playerLeft <= objectLeft && objectLeft <= playerRight)
+          ) {
+              clearInterval(fallingObjectInterval);
+              alert("Игра окончена!");
+              location.reload();
+          }
+
+          if (object.offsetTop >= gameContainer.offsetHeight) {
+              clearInterval(fallingObjectInterval);
+              gameContainer.removeChild(object);
+          } else {
+              object.style.top = object.offsetTop + 10 + "px";
+          }
+      }, 50);
+
+    // here we change amount of falling objects
+      setTimeout(createFallingObject, 100);
   }
+
+  createFallingObject();
+});
